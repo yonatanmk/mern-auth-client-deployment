c9f4cfa6e38d9b5fe97c8391d2e2f9d2520f9b09
yonatanmk <yonatanmk@gmail.com>
2020-02-26 16:53:15 -0500

    Access-Control-Allow-Origin header

--- src/lib/request.js
+++ src/lib/request.js
@@ -4,6 +4,7 @@ import { getLocalStorage } from 'lib/auth-utils'
 export default params => {
   const token = getLocalStorage('token');
   const headers = params.headers || {};
+  headers['Access-Control-Allow-Origin'] = process.env.REACT_APP_API;
   if (token) headers.Authorization = `Bearer ${token}`;
   return axios({
     ...params,
